8c358b8cd8291e3d93c4bc98d2574c4f30ac5ef4
azogue <eugenio.panadero@gmail.com>
2017-01-13 11:08:49 +0100

    no start & stop HA in config rsync (HA restart script now works so it's not necessary to do it here)

--- transfer.sh
+++ transfer.sh
@@ -4,9 +4,9 @@ REMOTE_HASS=/home/homeassistant/.homeassistant
 LOCAL_HASS=/Users/uge/Dropbox/PYTHON/PYPROJECTS/hass_config
 REMOTE_STATIC_PATH=/srv/homeassistant/homeassistant_venv/lib/python3.4/site-packages/homeassistant/components/frontend/www_static/
 
-echo Transfer HASS config from $LOCAL_HASS to $REMOTE_HASS in $HASSIP
-echo Stopping HASS...
-ssh pi@$HASSIP sudo systemctl stop home-assistant@homeassistant
+#echo Transfer HASS config from $LOCAL_HASS to $REMOTE_HASS in $HASSIP
+#echo Stopping HASS...
+#ssh pi@$HASSIP sudo systemctl stop home-assistant@homeassistant
 
 echo Set permissions for PI
 ssh pi@$HASSIP sudo chmod 777 -R $REMOTE_HASS
@@ -24,8 +24,8 @@ ssh pi@$HASSIP sudo chown homeassistant:homeassistant -R $REMOTE_HASS
 ssh pi@$HASSIP sudo chown homeassistant:homeassistant -R $REMOTE_STATIC_PATH/images
 ssh pi@$HASSIP sudo chmod 775 -R $REMOTE_HASS
 
-echo Starting HASS
-ssh pi@$HASSIP sudo systemctl start home-assistant@homeassistant
+#echo Starting HASS
+#ssh pi@$HASSIP sudo systemctl start home-assistant@homeassistant
 
 echo HASS LOG:
 ssh pi@$HASSIP sudo tail -n 100 $REMOTE_HASS/home-assistant.log
