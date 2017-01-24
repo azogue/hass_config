f454eb8d323de960b768f8572b71c18a92e875c3
azogue <eugenio.panadero@gmail.com>
2017-01-24 19:38:49 +0100

    hyperion light config selector; shell_commands out of config.yaml

--- switch_kodi_hyperion.sh
+++ switch_kodi_hyperion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HYPERION_IP=192.168.1.56
 HYPERION_USER=osmc
+CONF_ACTUAL=`ssh $HYPERION_USER@$HYPERION_IP cat /etc/hyperion/hyperion.config.json |grep "index"|wc -l`
 # Uses ssh with public-key in authorized_keys for secure-nopasswd login in remote kodi machine
 if [ "$1" = "" ]
 then
@@ -10,6 +11,20 @@ else
     if [ "$1" = "1" ]
     then
         ssh $HYPERION_USER@$HYPERION_IP sudo /usr/sbin/service hyperion start
+    elif [ "$1" = "set_hue4" ]
+    then
+        if [ ${CONF_ACTUAL} = '6' ]
+        then
+            ssh $HYPERION_USER@$HYPERION_IP sudo cp /home/osmc/hyperion_confs/hyperion_hue4.config.json /etc/hyperion/hyperion.config.json
+        fi
+        ssh $HYPERION_USER@$HYPERION_IP sudo /usr/sbin/service hyperion restart
+    elif [ "$1" = "set_hue6" ]
+    then
+        if [ ${CONF_ACTUAL} = '4' ]
+        then
+            ssh $HYPERION_USER@$HYPERION_IP sudo cp /home/osmc/hyperion_confs/hyperion_hue6.config.json /etc/hyperion/hyperion.config.json
+        fi
+        ssh $HYPERION_USER@$HYPERION_IP sudo /usr/sbin/service hyperion restart
     else
         ssh $HYPERION_USER@$HYPERION_IP sudo /usr/sbin/service hyperion stop
     fi
