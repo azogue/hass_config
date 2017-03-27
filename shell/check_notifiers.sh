32d0987b170f6e09909b5b92092ea59674b818ab
azogue <eugenio.panadero@gmail.com>
2017-03-27 16:00:06 +0200

    added OWNTRACKS device_tracker (and icloud disabled --> batt burner); added owntracks batt sensor for my iphone; added sensor for core error counter in HASS log

--- shell/check_notifiers.sh
+++ shell/check_notifiers.sh
@@ -20,16 +20,19 @@ elif [ "$1" = "kodi" ]
 then
     RESNUM=$(/bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c "Error setting up platform kodi")
     [ "$RESNUM" -lt "1" ]  && echo "success"||echo "fail"
+elif [ "$1" = "core_error" ]
+then
+    /bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.core
 elif [ "$1" = "core_warning" ]
 then
-    /bin/grep WARNING /home/homeassistant/.homeassistant/home-assistant.log | grep -c homeassistant.core
+    /bin/grep WARNING /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.core
 elif [ "$1" = "unable_find" ]
 then
-    /bin/grep WARNING /home/homeassistant/.homeassistant/home-assistant.log | grep -c "Unable to find service notify"
+    /bin/grep WARNING /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c "Unable to find service notify"
 else
-#    /bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | grep -c homeassistant.components.notify
-    RESCORE=$(/bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.core)
-    RESNOT=$(/bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.components.notify)
-    echo $((RESCORE + RESNOT))
+#    RESCORE=$(/bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.core)
+#    RESNOT=$(/bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.components.notify)
+#    echo $((RESCORE + RESNOT))
+    /bin/grep ERROR /home/homeassistant/.homeassistant/home-assistant.log | /bin/grep -c homeassistant.components.notify
 fi
 exit 0
