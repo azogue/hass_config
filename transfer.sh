ef6fda54bee6e816dc43f441a0ad0799cefabe0a
azogue <eugenio.panadero@gmail.com>
2017-04-22 10:53:55 +0200

    v 1.3.4 script.play_kodi_pvr to turn on TV; script.restart_kodi_osmc to kill kodi OSMC instance; transfer config script now save custom images in www/images (`/local/images/xxx.png`)

--- transfer.sh
+++ transfer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-HASSIP=192.168.1.13
+HASSIP=[REDACTED_HASSIP]
 REMOTE_HASS=/home/homeassistant/.homeassistant
-LOCAL_HASS=/Users/uge/Dropbox/PYTHON/PYPROJECTS/hass_config
-#REMOTE_STATIC_PATH=/srv/homeassistant/lib/python3.6/site-packages/homeassistant/components/frontend/www_static/
-REMOTE_STATIC_PATH=/home/homeassistant/.pyenv/versions/3.6.0/envs/hass36/lib/python3.6/site-packages/homeassistant/components/frontend/www_static/
+LOCAL_HASS=[REDACTED_LOCALPATH]
+REMOTE_STATIC_PATH=/home/homeassistant/.homeassistant/www/images/
 
 echo Set permissions for PI
 ssh pi@$HASSIP sudo chmod 777 -R $REMOTE_HASS
@@ -13,12 +12,11 @@ echo RSYNC:
 rsync -avrzp -L --no-o --no-g -ui  --stats --progress -e ssh --exclude-from $LOCAL_HASS/exclude_patterns.txt $LOCAL_HASS/ pi@$HASSIP:$REMOTE_HASS
 
 echo COPY CUSTOM IMAGES TO STATIC in $REMOTE_STATIC_PATH:
-ssh pi@$HASSIP sudo cp $REMOTE_HASS/custom_images/* $REMOTE_STATIC_PATH/images/
+ssh pi@$HASSIP sudo cp $REMOTE_HASS/custom_images/* $REMOTE_STATIC_PATH
 ssh pi@$HASSIP ls -la $REMOTE_STATIC_PATH/images/
 
 echo Set permissions for homeassistant
 ssh pi@$HASSIP sudo chown homeassistant:homeassistant -R $REMOTE_HASS
-ssh pi@$HASSIP sudo chown homeassistant:homeassistant -R $REMOTE_STATIC_PATH/images
 ssh pi@$HASSIP sudo chmod 775 -R $REMOTE_HASS
 
 echo HASS LOG:
