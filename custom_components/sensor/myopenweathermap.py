e7081d0267c2e71c1207f432afea03fddc9b0ffe
azogue <eugenio.panadero@gmail.com>
2017-04-26 19:28:49 +0200

    v 1.5 Custom version of the openweathermap sensor.  --> More error handling getting forecast

--- custom_components/sensor/myopenweathermap.py
+++ custom_components/sensor/myopenweathermap.py
@@ -190,6 +190,9 @@ class WeatherData(object):
         self.data = obs.get_weather()
 
         if self.forecast == 1:
-            obs = self.owm.three_hours_forecast_at_coords(
-                self.latitude, self.longitude)
-            self.fc_data = obs.get_forecast()
+            try:
+                obs = self.owm.three_hours_forecast_at_coords(
+                    self.latitude, self.longitude)
+                self.fc_data = obs.get_forecast()
+            except TypeError:
+                _LOGGER.warning("Failed to fetch OpenWeatherMap forecast")
