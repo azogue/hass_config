c422c285108bd9b5ad70b8a2b3818006e48d168f
azogue <eugenio.panadero@gmail.com>
2017-06-16 18:09:12 +0200

    i2c sensors using new i2csense library

--- custom_components/sensor/bh1750.py
+++ custom_components/sensor/bh1750.py
@@ -4,8 +4,8 @@ Support for BH1750 light sensor.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.bh1750/
 """
-import time
 import asyncio
+from functools import partial
 import logging
 
 import voluptuous as vol
@@ -15,13 +15,15 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
 
-REQUIREMENTS = ['smbus-cffi==0.5.1']
+REQUIREMENTS = ['i2csense==0.0.2',
+                'smbus-cffi==0.5.1']
 
 _LOGGER = logging.getLogger(__name__)
 
 CONF_I2C_ADDRESS = 'i2c_address'
 CONF_I2C_BUS = 'i2c_bus'
 CONF_OPERATION_MODE = 'operation_mode'
+CONF_SENSITIVITY = 'sensitivity'
 CONF_DELAY = 'measurement_delay_ms'
 
 # Operation modes for BH1750 sensor (from the datasheet). Time typically 120ms
@@ -47,6 +49,7 @@ DEFAULT_I2C_ADDRESS = '0x23'
 DEFAULT_I2C_BUS = 1
 DEFAULT_MODE = CONTINUOUS_HIGH_RES_MODE_1
 DEFAULT_DELAY_MS = 120
+DEFAULT_SENSITIVITY = 69  # from 31 to 254
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -54,6 +57,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_I2C_BUS, default=DEFAULT_I2C_BUS): vol.Coerce(int),
     vol.Optional(CONF_OPERATION_MODE, default=DEFAULT_MODE):
         vol.In(OPERATION_MODES),
+    vol.Optional(CONF_SENSITIVITY, default=DEFAULT_SENSITIVITY):
+        cv.positive_int,
     vol.Optional(CONF_DELAY, default=DEFAULT_DELAY_MS): cv.positive_int,
 })
 
@@ -64,19 +69,25 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the BH1750 sensor."""
     name = config.get(CONF_NAME)
     bus_number = config.get(CONF_I2C_BUS)
-    i2c_address = int(config.get(CONF_I2C_ADDRESS), 0)
+    i2c_address = config.get(CONF_I2C_ADDRESS)
     operation_mode = config.get(CONF_OPERATION_MODE)
-    measurement_delay = config.get(CONF_DELAY)
 
     try:
         # noinspection PyUnresolvedReferences
         import smbus
+        from i2csense.bh1750 import BH1750
         bus = smbus.SMBus(bus_number)
     except ImportError as exc:
         _LOGGER.error("ImportError: %s", exc)
         return False
 
-    sensor = BH1750(bus, i2c_address, operation_mode, measurement_delay)
+    sensor = yield from hass.async_add_job(
+        partial(BH1750, bus, i2c_address,
+                operation_mode=operation_mode,
+                measurement_delay=config.get(CONF_DELAY),
+                sensitivity=config.get(CONF_SENSITIVITY),
+                logger=_LOGGER)
+    )
     if not sensor.sample_ok:
         _LOGGER.error("BH1750 sensor not detected at %s", i2c_address)
         return False
@@ -87,122 +98,14 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     async_add_devices(dev)
 
 
-class BH1750:
-    """Implement BH1750 communication."""
-
-    # Define some constants from the datasheet
-    POWER_DOWN = 0x00  # No active state
-    POWER_ON = 0x01  # Power on
-    RESET = 0x07  # Reset data register value
-
-    def __init__(self, bus, addr=int(DEFAULT_I2C_ADDRESS, 0),
-                 operation_mode=DEFAULT_MODE,
-                 measurement_delay=DEFAULT_DELAY_MS):
-        """Initialize the sensor."""
-        self._ok = False
-        self._bus = bus
-        self._i2c_add = addr
-        self._mode = None
-        self._delay = measurement_delay / 1000.
-        self._operation_mode = OPERATION_MODES[operation_mode][0]
-        self._continuous_sampling = OPERATION_MODES[operation_mode][1]
-        self._power_down()
-        self._mtreg = None
-        self.set_sensitivity()
-        self.light_level = -1
-        self.update()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """Shut down the sensor at exit."""
-        self._power_down()
-
-    def _set_mode(self, mode):
-        self._mode = mode
-        try:
-            self._bus.write_byte(self._i2c_add, self._mode)
-            self._ok = True
-        except OSError as exc:
-            _LOGGER.error("Bad writing in bus: %s", exc)
-            self._ok = False
-
-    def _power_down(self):
-        self._set_mode(self.POWER_DOWN)
-
-    def _power_on(self):
-        self._set_mode(self.POWER_ON)
-
-    def _reset(self):
-        # It has to be powered on before resetting
-        self._power_on()
-        self._set_mode(self.RESET)
-
-    @property
-    def sample_ok(self) -> bool:
-        """Return sensor ok state."""
-        return self._ok
-
-    @property
-    def sensitivity(self) -> int:
-        """Return the sensitivity value, an integer between 31 and 254."""
-        return self._mtreg
-
-    def set_sensitivity(self, sensitivity=69):
-        """Set the sensitivity value.
-
-        Valid values are 31 (lowest) to 254 (highest), default is 69.
-        """
-        if sensitivity < 31:
-            self._mtreg = 31
-        elif sensitivity > 254:
-            self._mtreg = 254
-        else:
-            self._mtreg = sensitivity
-        self._power_on()
-        self._set_mode(0x40 | (self._mtreg >> 5))
-        self._set_mode(0x60 | (self._mtreg & 0x1f))
-        self._power_down()
-
-    def _get_result(self) -> float:
-        """Return current measurement result in lx."""
-        try:
-            data = self._bus.read_word_data(self._i2c_add, self._mode)
-            self._ok = True
-        except OSError as exc:
-            _LOGGER.error("Bad reading in bus: %s", exc)
-            self._ok = False
-            return -1
-
-        count = data >> 8 | (data & 0xff) << 8
-        mode2coeff = 2 if (self._mode & 0x03) == 0x01 else 1
-        ratio = 1 / (1.2 * (self._mtreg / 69.0) * mode2coeff)
-        return ratio * count
-
-    def _wait_for_result(self):
-        """Wait for the sensor to be ready for measurement."""
-        basetime = 0.018 if (self._mode & 0x03) == 0x03 else 0.128
-        time.sleep(basetime * (self._mtreg / 69.0) + self._delay)
-
-    def update(self):
-        """Update the measured light level in lux."""
-        if not self._continuous_sampling \
-                or self.light_level < 0 \
-                or self._operation_mode != self._mode:
-            self._reset()
-            self._set_mode(self._operation_mode)
-            self._wait_for_result()
-        self.light_level = self._get_result()
-        _LOGGER.debug('Measurement mode: %s, ∆s: %s, last: %.1f',
-                      self._operation_mode, self._delay, self.light_level)
-
-
 class BH1750Sensor(Entity):
     """Implementation of the BH1750 sensor."""
 
-    def __init__(self, bh1750_client, name, unit):
+    def __init__(self, bh1750_sensor, name, unit):
         """Initialize the sensor."""
         self._name = name
         self._unit_of_measurement = unit
-        self.bh1750_client = bh1750_client
+        self.bh1750_sensor = bh1750_sensor
         self._state = None
 
     @property
@@ -225,13 +128,13 @@ class BH1750Sensor(Entity):
         """Return the class of this device, from component DEVICE_CLASSES."""
         return 'light'
 
-    # def update(self):
     @asyncio.coroutine
     def async_update(self):
         """Get the latest data from the BH1750 and update the states."""
-        yield from self.hass.async_add_job(self.bh1750_client.update)
-        if self.bh1750_client.light_level >= 0:
-            self._state = int(round(self.bh1750_client.light_level))
+        yield from self.hass.async_add_job(self.bh1750_sensor.update)
+        if self.bh1750_sensor.sample_ok \
+                and self.bh1750_sensor.light_level >= 0:
+            self._state = int(round(self.bh1750_sensor.light_level))
         else:
             _LOGGER.warning("Bad Update of sensor.%s: %s",
-                            self.name, self.bh1750_client.light_level)
+                            self.name, self.bh1750_sensor.light_level)
